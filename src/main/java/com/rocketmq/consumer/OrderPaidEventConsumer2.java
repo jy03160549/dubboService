d40e9d567a494694ae9e04c49108f87d1cd4fb6c
jy03160549 <328935410@qq.com>
2018-09-28 22:25:35 +0800

    添加rabbitmq

--- src/main/java/com/rocketmq/consumer/OrderPaidEventConsumer2.java
+++ src/main/java/com/rocketmq/consumer/OrderPaidEventConsumer2.java
@@ -2,13 +2,13 @@ package com.rocketmq.consumer;
 //import org.apache.rocketmq.spring.starter.annotation.RocketMQMessageListener;
 //import org.apache.rocketmq.spring.starter.core.RocketMQListener;
 //import org.springframework.stereotype.Service;
-//
+
 //@Service
 //@RocketMQMessageListener(topic = "order-paid-topic", consumerGroup = "order-paid-consumer")
-//public class OrderPaidEventConsumer implements RocketMQListener<OrderPaidEvent> {
+//public class OrderPaidEventConsumer2 implements RocketMQListener<OrderPaidEvent> {
 //
 //    @Override
 //    public void onMessage(OrderPaidEvent orderPaidEvent) {
-//    	System.out.println(orderPaidEvent.getOrderId()+"  "+orderPaidEvent.getPaidMoney());
+//    	System.out.println(orderPaidEvent.getOrderId()+" OrderPaidEventConsumer2 "+orderPaidEvent.getPaidMoney());
 //    }
 //}
--- src/main/java/com/rocketmq/consumer/StringConsumer.java
+++ src/main/java/com/rocketmq/consumer/StringConsumer.java
@@ -1,17 +1,17 @@
 package com.rocketmq.consumer;
 
-import org.apache.rocketmq.spring.starter.annotation.RocketMQMessageListener;
-import org.apache.rocketmq.spring.starter.core.RocketMQListener;
-import org.springframework.stereotype.Service;
-
-/**
- * StringConsumer Created by aqlu on 2017/11/16.
- */
-@Service
-@RocketMQMessageListener(topic = "string-topic", consumerGroup = "string_consumer")
-public class StringConsumer implements RocketMQListener<String> {
-    @Override
-    public void onMessage(String message) {
-        System.out.println(message);
-    }
-}
+//import org.apache.rocketmq.spring.starter.annotation.RocketMQMessageListener;
+//import org.apache.rocketmq.spring.starter.core.RocketMQListener;
+//import org.springframework.stereotype.Service;
+//
+///**
+// * StringConsumer Created by aqlu on 2017/11/16.
+// */
+//@Service
+//@RocketMQMessageListener(topic = "string-topic", consumerGroup = "string_consumer")
+//public class StringConsumer implements RocketMQListener<String> {
+//    @Override
+//    public void onMessage(String message) {
+//        System.out.println(message);
+//    }
+//}
